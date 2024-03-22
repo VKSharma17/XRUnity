425c2de5e3db65f0e631c546316d456153ed47e7
Vinay Krishna Sharma <54863987+VKSharma17@users.noreply.github.com>
2024-03-22 13:35:41 +0530

    AR Dragon Project Scripts

---
+++ ARDragon/PrefabController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PrefabController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject dragonPrefab;
+    [SerializeField] private Vector3 prefabOffset;
+
+    private GameObject dragon;
+    private ARTrackedImageManager aRTrackedImageManager;
+
+    private void OnEnable()
+    {
+        aRTrackedImageManager = gameObject.GetComponent<ARTrackedImageManager>();
+        aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs obj)
+    {
+        foreach(ARTrackedImage image in obj.added)
+        {
+            dragon = Instantiate(dragonPrefab,image.transform);
+            dragon.transform.position += prefabOffset;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
